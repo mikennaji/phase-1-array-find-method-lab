a2c74d1fb73cca5693f5675c252e49ed02a7e6f1
mikennaji <mnnaji3@uwo.ca>
2022-03-26 21:02:31 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin (record){
+     const result = record.find(element => element.result==='W');
+     return result !== undefined ? result.year : result;
+}
+;
+
+
+
+    
+
+
